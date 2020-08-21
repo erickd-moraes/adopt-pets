87656b80c7a8f919de5d24629cb782c16115b9db
Erick Moraes <erickd.moraes@gmail.com>
2020-08-20 22:46:21 -0300

    Component Header and Section About created and styled

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link }  from 'react-router-dom';
+import Logo from '../../assets/images/logo.svg';
+
+import './styles.css';
+
+const Header = () => {
+  return (
+    <header>
+      <Link to="/">
+        <img src={Logo} alt="Logo Adopt Pets"/>
+      </Link>
+      <nav className="menu">
+        <ul>
+          <li>
+            <a href="#about">Sobre a iniciativa</a>
+          </li>
+          <li>
+            <a href="#find-pets">Procurar pets</a>
+          </li>
+          <li>
+            <Link to="/sign-in" className="sign-in">Entrar</Link>
+          </li>
+          <li>
+            <Link to="/sign-up" className="sign-up">Cadastre-se</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import Header from '../../components/Header';
+import ImageAbout from '../../assets/images/background-about.svg';
 
 import './styles.css';
 
 const Home = () => {
-  return <h1>Home Page</h1>
+  return (
+    <div id="home-page">
+      <Header />
+
+      <main>
+        <section id="about">
+          <div>
+            <h1>Sobre a iniciativa</h1>
+            <p>
+              O <strong className="highlight">AdoptPets</strong> surgiu com o intuito único e genuíno de conseguir <strong>ajudar
+              animais de rua, maltratados ou abandonados a encontrarem um lar onde
+              serão amados, recebam muito carinho e sejam  cuidados da maneira que merecem!</strong>
+              <br/>
+              <strong className="highlight">E que forma seria essa?</strong> Uma plataforma online, servindo como um facilitador,
+              possibilitando a conexão entre pessoas que simpatizam com esse pensamento e os
+              pets que tanto precisam desse âmparo.
+            </p>
+          </div>
+
+          <img src={ImageAbout} alt="Sobre a iniciativa"/>
+        </section>
+      </main>
+    </div>
+  );
 };
 
 export default Home;
