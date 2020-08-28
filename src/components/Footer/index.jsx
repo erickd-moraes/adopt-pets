73f6b38e275aa6abfbd0a4833302c849703f05e2
Erick Moraes <erickd.moraes@gmail.com>
2020-08-27 21:47:03 -0300

    Fixed header rendering error

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import Logo from '../../assets/images/logo.svg';
 import iconLinkedIn from '../../assets/images/icon-linkedin.svg';
 import iconGitHub from '../../assets/images/icon-github.svg';
@@ -13,56 +14,53 @@ const Footer = () => {
     <>
       <footer>
         <div>
-          <img src={Logo} alt="Logo Adopt Pets"/>
+            <img src={Logo} alt="Logo Adopt Pets"/>
         </div>
 
-        <div className="menu">
-          <p>Quer entrar em contato com o criador ou conhecê-lo melhor?</p>
-          <nav>
-            <ul>
-              <li>
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="https://www.linkedin.com/in/erickd-mooraes/">
-                  <img src={iconLinkedIn} alt="Ícone LinkedIn"/>
-                </a>
-              </li>
-              <li>
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="https://github.com/erickd-moraes">
-                  <img src={iconGitHub} alt="Ícone GitHub"/>
-                </a>
-              </li>
-              <li>
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="#whatsapp">
-                  <img src={iconWhatssapp} alt="Ícone Whatsapp"/>
-                </a>
-              </li>
-              <li>
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="https://www.facebook.com/erick.moraes.184/">
-                  <img src={iconFacebook} alt="Ícone Facebook"/>
-                </a>
-              </li>
-              <li>
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="https://www.instagram.com/erick.mooraes/">
-                  <img src={iconInstagram} alt="Ícone Instagram"/>
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </div>
+        <nav>
+          <ul>
+            <li>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.linkedin.com/in/erickd-mooraes/">
+                <img src={iconLinkedIn} alt="Ícone LinkedIn"/>
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://github.com/erickd-moraes">
+                <img src={iconGitHub} alt="Ícone GitHub"/>
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="#whatsapp">
+                <img src={iconWhatssapp} alt="Ícone Whatsapp"/>
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.facebook.com/erick.moraes.184/">
+                <img src={iconFacebook} alt="Ícone Facebook"/>
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.instagram.com/erick.mooraes/">
+                <img src={iconInstagram} alt="Ícone Instagram"/>
+              </a>
+            </li>
+          </ul>
+        </nav>
       </footer>
     </>
   );
